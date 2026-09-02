c2379769ea72fffecf8c57b01a7c5c3a99334c39
mnasa-codex <jasakhadsin@gmail.com>
2026-09-02 20:08:26 +0300

    Switch home entry to WebGL and 3D tilt

--- src/js/entry-home.js
+++ src/js/entry-home.js
@@ -1,17 +1,9 @@
 import { $ } from './lib/dom.js';
 import { initTheme } from './modules/theme.js';
 import { initReveal } from './modules/reveal.js';
-import { initSpotlight } from './modules/spotlight.js';
-import { initMorphFigure } from './modules/morph-figure.js';
+import { initWebglStage } from './modules/webgl-stage.js';
+import { initTilt } from './modules/tilt.js';
 import { initScrollProgress, initToTop } from './modules/scroll-progress.js';
 import { initFab } from './modules/fab.js';
 import { initLangSwitch } from './modules/lang-switch.js';
-
-initTheme($('[data-theme-toggle]'));
-initLangSwitch();
-initReveal();
-initSpotlight();
-initMorphFigure($('[data-morph-figure]'));
-initScrollProgress($('.read-progress'));
-initToTop($('.to-top'));
-initFab();
+initTheme($('[data-theme-toggle]'));initLangSwitch();initReveal();initWebglStage($('[data-orb]'));initTilt();initScrollProgress($('.read-progress'));initToTop($('.to-top'));initFab();
